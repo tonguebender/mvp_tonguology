e263705115c0535057f3a2d59243eaf9cead9587
Sergey Maksimov <smaksimoff@gmail.com>
2018-01-26 19:33:58 +0100

    add create user route

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 require('./lib/server');
+
+// Process management stuff
+const db = require('./lib/db');
+
+// todo: needs improvement
+process.on('SIGINT', function() {
+  db.closeConnection(() => {
+    console.log('Disconnected from mongodb');
+    process.exit(0);
+  });
+});
+// nodemon restart hacks todo: wrap in dev check
+process.once('SIGUSR2', function() {
+  db.closeConnection(() => {
+    console.log('Disconnected from mongodb');
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
